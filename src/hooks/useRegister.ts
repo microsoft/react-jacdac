7d3994f0605f3d21c689a21ff600e9ba91c5003c
pelikhan <jhalleux@microsoft.com>
2021-10-26 14:09:00 -0700

    reorder imports

--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -1,6 +1,12 @@
 import { JDService } from "jacdac-ts"
 import { useMemo } from "react"
 
+/**
+ * A hook that resolves the register of a service, given the register code.
+ * @param service service to query
+ * @param identifier register code
+ * @returns register JDOM node, if supported in the specification
+ */
 export function useRegister(service: JDService, identifier: number) {
     return useMemo(() => service?.register(identifier), [service, identifier])
 }
