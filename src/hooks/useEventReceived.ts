6fbb4cf3ff0f88a233b4afca98ba75a6df5a328d
pelikhan <jhalleux@microsoft.com>
2021-10-27 09:22:47 -0700

    patch: add event tracking

---
+++ src/hooks/useEventReceived.ts
@@ -0,0 +1,15 @@
+import { EVENT, JDEvent } from "jacdac-ts"
+import { DependencyList, useEffect } from "react"
+
+/**
+ * Registers a handler to run whenever the event is received from the bus
+ * @param event event to listen
+ * @param handler handler, not part of depenency list
+ */
+export function useEventReceived(
+    event: JDEvent,
+    handler: (event: JDEvent) => void,
+    deps?: DependencyList
+) {
+    useEffect(() => event?.subscribe(EVENT, handler), [event, ...(deps || [])])
+}
