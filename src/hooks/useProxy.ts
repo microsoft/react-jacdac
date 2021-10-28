45dbb4fd617010f343764fcc1da9632a239ce32c
pelikhan <jhalleux@microsoft.com>
2021-10-27 17:06:06 -0700

    patch: add proxy handling

---
+++ src/hooks/useProxy.ts
@@ -0,0 +1,22 @@
+import { ControlCmd, DEVICE_ANNOUNCE, Packet, SRV_CONTROL } from "jacdac-ts"
+import { useEffect } from "react"
+import { useBus } from "./useBus"
+
+/**
+ * A hook to force brains into proxy mode.
+ * @param force
+ */
+export default function useProxy(force: boolean) {
+    const bus = useBus()
+    useEffect(() => {
+        if (!force) return
+
+        const forceProxy = () => {
+            const pkt = Packet.onlyHeader(ControlCmd.Proxy)
+            pkt.sendAsMultiCommandAsync(bus, SRV_CONTROL)
+        }
+        const unsub = bus.subscribe(DEVICE_ANNOUNCE, forceProxy)
+        forceProxy()
+        return unsub
+    }, [force])
+}
