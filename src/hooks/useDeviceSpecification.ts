cc47415ea028fe35d8eb85da3cf68df0976ecd6e
pelikhan <jhalleux@microsoft.com>
2022-02-03 06:40:59 -0800

    add device spec hooks

--- src/hooks/useDeviceSpecification.ts
+++ src/hooks/useDeviceSpecification.ts
@@ -16,10 +16,24 @@ export function useDeviceSpecification(
     deps?: DependencyList
 ) {
     const id = useDeviceProductIdentifier(device, options, deps)
+    return useDeviceSpecificationFromProductIdentifier(id)
+}
+
+export function useDeviceSpecificationFromProductIdentifier(id: number) {
     const deviceCatalog = useDeviceCatalog()
     const specification = useChange(
         deviceCatalog,
         _ => _.specificationFromProductIdentifier(id),
+        [id]
+    )
+    return specification
+}
+
+export function useDeviceSpecificationFromIdentifier(id: string) {
+    const deviceCatalog = useDeviceCatalog()
+    const specification = useChange(
+        deviceCatalog,
+        _ => _.specificationFromIdentifier(id),
         [id]
     )
     return specification
