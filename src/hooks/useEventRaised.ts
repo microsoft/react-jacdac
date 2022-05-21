6f0f0caf023f41fced4198a10399121ec3dbfaf7
pelikhan <jhalleux@microsoft.com>
2022-05-21 08:48:14 -0700

    updated deps

--- src/hooks/useEventRaised.ts
+++ src/hooks/useEventRaised.ts
@@ -1,25 +1,47 @@
-import { IEventSource } from "jacdac-ts"
+import { IEventSource, arrayEq } from "jacdac-ts"
 import { useMemo, DependencyList } from "react"
-import { Subscription, useSubscription } from "use-subscription"
+import { useSyncExternalStoreWithSelector } from "use-sync-external-store/with-selector"
 
-export function useEventRaised<TEventSource extends IEventSource, TValue>(
+/**
+ * A hook to track event and update a state snapshot
+ */
+export default function useEventRaised<
+    TEventSource extends IEventSource,
+    TValue
+>(
     eventName: string | string[],
     node: TEventSource,
-    query?: (n: TEventSource) => TValue,
-    deps?: DependencyList
+    query: (n: TEventSource) => TValue,
+    deps?: DependencyList,
+    isEqual?: (a: TValue, b: TValue) => boolean
 ): TValue {
     const subscription = useMemo(
-        () =>
-            <Subscription<TValue>>{
-                getCurrentValue: query
-                    ? () => query(node)
-                    : () => <TValue>undefined,
-                subscribe: callback => {
-                    const unsubscribe = node?.subscribe(eventName, callback)
-                    return () => unsubscribe?.()
-                },
+        () => ({
+            getSnapshot: () => query?.(node),
+            selector: _ => _,
+            subscribe: (onStoreChanged: () => void) => {
+                const unsubscribe = node?.subscribe(eventName, onStoreChanged)
+                return () => unsubscribe?.()
             },
+            isEqual: isEqual
+                ? isEqual
+                : (a: TValue, b: TValue) => {
+                      if (Array.isArray(a)) {
+                          const e = arrayEq(
+                              a as unknown as unknown[],
+                              b as unknown as unknown[]
+                          )
+                          return e
+                      } else return Object.is(a, b)
+                  },
+        }),
         [node, ...(deps || [])]
     )
-    return useSubscription(subscription)
+    return useSyncExternalStoreWithSelector(
+        subscription.subscribe,
+        subscription.getSnapshot,
+        undefined,
+        subscription.selector,
+        subscription.isEqual
+    )
 }
